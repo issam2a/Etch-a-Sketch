b3dd17781a95f05d2262e49799c27694c5fb59f0
issam <issamissa2a@gmail.com>
2023-10-11 14:29:22 +0300

    we can append the entire fragment to the live DOM tree,     //  typically by appending it to a container element.

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -5,3 +5,14 @@ let gridSize = 16;
 let divNum = Math.pow(gridSize, 2);
 let showUserInput = document.querySelector(".label");
 showUserInput.textContent = `Grid size : ${gridSize}x${gridSize}`;
+
+function createGridDivs() {
+  for (let i = 0; i < divNum; i++) {
+    let innerDiv = document.createElement("div");
+    innerDiv.classList.add("innerDiv");
+
+    fragment.appendChild(innerDiv);
+  }
+
+  container.appendChild(fragment);
+}
