8c139ba76227188b859b82f983c967d69f27f07b
issam <issamissa2a@gmail.com>
2023-10-11 14:28:07 +0300

    defining the initial value that determine the size of the grid and then according to the grid size // we can set the number of divs should be created

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -1,3 +1,7 @@
 let container = document.querySelector(".container");
 let userInput = document.querySelector('input[type="range"]');
 let fragment = document.createDocumentFragment();
+let gridSize = 16;
+let divNum = Math.pow(gridSize, 2);
+let showUserInput = document.querySelector(".label");
+showUserInput.textContent = `Grid size : ${gridSize}x${gridSize}`;
